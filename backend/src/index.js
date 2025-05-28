3afeb8403c0c5163e21242c2a04e6f50211aa7da
Subhash <146863118+subhashvadrevu@users.noreply.github.com>
2025-05-29 00:46:24 +0530

    second commit

---
+++ backend/src/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+
+import authRoutes from "./routes/auth.routes.js";
+import problemRoutes from "./routes/problem.routes.js";
+import submissionRoutes from "./routes/submission.routes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    console.log('hello, welcome to codepy... ❤️')
+});
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/problems", problemRoutes);
+app.use("/api/v1/submit", submissionRoutes);
+
+app.listen(process.env.PORT, () => {
+    console.log('server is running on prot 5555');
+});
