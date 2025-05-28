ab2f1faa3ea85b1e4fe19e1fd99a6ce3ba781c67
Subhash <146863118+subhashvadrevu@users.noreply.github.com>
2025-05-29 00:47:14 +0530

    added auth routes

---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { login, logout, me, register } from "../controllers/auth.controllers.js";
+import { authMiddleware } from "../middlewares/auth.middlewares.js";
+
+
+const authRoutes = express.Router();
+
+
+authRoutes.post("/register", register);
+
+authRoutes.post("/login", login);
+
+authRoutes.post("/logout", authMiddleware, logout);
+
+authRoutes.get("/me", authMiddleware, me);
+
+
+export default authRoutes;
