f52121355f6fd4366cd8436e63fee60e921b03be
Subhash <146863118+subhashvadrevu@users.noreply.github.com>
2025-05-29 00:49:44 +0530

    added auth middlewares

---
+++ backend/src/middlewares/auth.middlewares.js
@@ -0,0 +1,80 @@
+import jwt from "jsonwebtoken";
+import  { db } from "../libs/db.js";
+import { UserRole } from "../generated/prisma/index.js";
+
+export const authMiddleware = async(req, res, next) => {
+    try {
+        const token = req.cookies.jwt;
+
+        if(!token) {
+            return res.status(401).json({
+                message: "Unauthorized Access - No token provided"
+            })
+        }
+
+        let decoded;
+
+        try {
+            decoded = jwt.verify(token, process.env.JWT_SECRET);
+        } catch (error) {
+            return res.status(401).json({
+                message: "Unauthorized Access - Invalid token"
+            })
+        }
+
+        const user = await db.user.findUnique({
+            where: {
+                id: decoded.id
+            },
+            select: {
+                id: true,
+                name: true, 
+                email: true,
+                role: true
+            }
+        });
+
+        if(!user) {
+            return res.status(404).json({
+                message: "User not found"
+            })
+        }
+
+        req.user = user;
+        next();
+        
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            error: "Error authenticating user"
+        })
+    }
+};
+
+export const checkAdmin = async(req, res, next) => {
+    try {
+        const userId = req.user.id;
+        const user = await db.user.findUnique({
+            where: {
+                id: userId
+            },
+            select: {
+                role: true
+            }
+        })
+
+        if(!user || user.role !== UserRole.ADMIN) {
+            return res.status(403).json({
+                message: "Access denied"
+            })
+        }
+
+        next();
+
+    } catch (error) {
+        console.log("Admin check : ",error);
+        return res.status(500).json({
+            error: "Error checking user role"
+        })
+    }
+}
