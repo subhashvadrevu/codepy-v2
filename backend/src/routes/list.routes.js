7d84443ba62bcd3eb509bc7b3285b5ceca46e73b
Subhash <146863118+subhashvadrevu@users.noreply.github.com>
2025-05-30 18:34:42 +0530

    added list routes

---
+++ backend/src/routes/list.routes.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { authMiddleware } from "../middlewares/auth.middlewares.js";
+import { addProblemToList, createList, deleteList, getAllListDetails, getListById, removeProblemFromList } from "../controllers/list.controllers";
+
+const listRoutes = express.Router();
+
+listRoutes.post("/create", authMiddleware, createList);
+
+listRoutes.post("/:id/addProblem",authMiddleware, addProblemToList);
+
+listRoutes.delete("/delete/:id", authMiddleware, deleteList);
+
+listRoutes.delete("/delete/:id/removeProblem", authMiddleware, removeProblemFromList);
+
+listRoutes.get("/", authMiddleware, getAllListDetails); 
+
+listRoutes.get("/:id", authMiddleware, getListById);
+
+export default listRoutes;
