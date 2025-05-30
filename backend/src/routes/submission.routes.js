b43045e70524cfdfbd46485caf19d2682cc1760d
Subhash <146863118+subhashvadrevu@users.noreply.github.com>
2025-05-30 17:04:12 +0530

    added submission routes

--- backend/src/routes/submission.routes.js
+++ backend/src/routes/submission.routes.js
@@ -1,10 +1,16 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middlewares.js";
-import { submitCode } from "../controllers/submission.controllers.js";
+import { getAllSubmissions, getSubmissions, getSubmissionsCount, submitCode } from "../controllers/submission.controllers.js";
 
 const submissionRoutes = express.Router();
 
 submissionRoutes.post("/", authMiddleware, submitCode);
+
+submissionRoutes.get("/getAllSubmissions", authMiddleware, getAllSubmissions);
+
+submissionRoutes.get("/getSubmissions/:id", authMiddleware, getSubmissions);
+
+submissionRoutes.get("/getSubmissionCount/:id", authMiddleware, getSubmissionsCount);
 
 
 export default submissionRoutes;
