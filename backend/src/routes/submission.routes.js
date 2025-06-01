789639d5baf4b5dada920e05e448a3bfe3c5ba0f
Subhash <146863118+subhashvadrevu@users.noreply.github.com>
2025-06-01 23:48:45 +0530

    fixed controller names

--- backend/src/routes/submission.routes.js
+++ backend/src/routes/submission.routes.js
@@ -1,16 +1,17 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middlewares.js";
-import { getAllSubmissions, getSubmissions, getSubmissionsCount, submitCode } from "../controllers/submission.controllers.js";
+import { getAllSubmissionsByUser, getSubmissions, getSubmissionsCountForProblemByAllUsers, getTotalSubmissionsCountByUser, submitCode } from "../controllers/submission.controllers.js";
 
 const submissionRoutes = express.Router();
 
 submissionRoutes.post("/", authMiddleware, submitCode);
 
-submissionRoutes.get("/getAllSubmissions", authMiddleware, getAllSubmissions);
+submissionRoutes.get("/getAllSubmissions", authMiddleware, getAllSubmissionsByUser);
 
 submissionRoutes.get("/getSubmissions/:id", authMiddleware, getSubmissions);
 
-submissionRoutes.get("/getSubmissionCount/:id", authMiddleware, getSubmissionsCount);
+submissionRoutes.get("/getSubmissionCount/:id", getSubmissionsCountForProblemByAllUsers);
 
+submissionRoutes.get("/getTotalSubmissionsByUser/:username", getTotalSubmissionsCountByUser);
 
 export default submissionRoutes;
