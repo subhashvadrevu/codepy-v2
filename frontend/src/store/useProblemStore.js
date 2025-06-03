1f1f030271d02d164978d65f9dc295ef82d51cf2
Subhash <146863118+subhashvadrevu@users.noreply.github.com>
2025-06-03 23:20:38 +0530

    problems global context

---
+++ frontend/src/store/useProblemStore.js
@@ -0,0 +1,63 @@
+import { create } from "zustand";
+import { axiosInstance } from "@/utilities/axios";
+import toast from "react-hot-toast";    
+
+
+export const useProblemStore = create((set) => ({
+    problems: [],
+    problem: null,
+    solvedProblems: [],
+    isProblemsLoading: false,
+    isProblemLoading: false,
+
+    getAllProblems: async() => {
+        try {
+            set({ isProblemsLoading: true });
+
+            const res = await axiosInstance.get("/problems/getAllProblems");
+            console.log("fetched all problems : ", res.data);
+
+            set({ problems: res.data.problems });
+
+        } catch (error) {
+            console.log("error fetching all problems : ", error);
+            toast.error("Error fetching problems");
+        } finally {
+            set({ isProblemsLoading: false });
+        }
+    },
+
+    getProblemById: async(id) => {
+        try {
+            set({ isProblemLoading: true });
+
+            const res = await axiosInstance.get(`/problems/getProblem/${id}`);
+            console.log("fetched problem: ", res.data);
+
+            set({ problem: res.data.problem });
+
+
+        } catch (error) {
+            console.log("error fetching problem :", error);
+            toast.error("Error fetching problem with given id");
+        } finally {
+            set({ isProblemLoading: false });
+        }
+    },
+
+    getSolvedProblemsByUser: async() => {
+        try {
+            const res = await axiosInstance.get("/problems/getSolvedProblems");
+            console.log("solved problems of user: ", res.data);
+
+            set({ solvedProblems: res.data.problems });
+
+            // toast.success("Solved problems fetched successfully");
+
+        } catch (error) {
+            console.log("Error fetching solved problems by user: ", error);
+            toast.error("Error fetching solved problems by user");
+        }
+    }
+
+}));
