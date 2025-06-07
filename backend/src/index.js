9089114c2ace650de7ad3b9502a3bff094716d64
Subhash <146863118+subhashvadrevu@users.noreply.github.com>
2025-06-07 16:39:35 +0530

    abc

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "*",
     credentials: true
 }));
 
