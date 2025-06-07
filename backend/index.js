e9e13e81a153dc7bbe68226528fe0cc02b351b19
Subhash <146863118+subhashvadrevu@users.noreply.github.com>
2025-06-07 18:30:18 +0530

    fixes

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://codepy-v2.vercel.app/",
+    origin: "https://codepy-v2.vercel.app",
     credentials: true
 }));
 
