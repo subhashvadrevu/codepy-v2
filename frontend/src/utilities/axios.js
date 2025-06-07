2da5c5386e00e5bc5b004372131a48e61ca83034
Subhash <146863118+subhashvadrevu@users.noreply.github.com>
2025-06-07 18:26:12 +0530

    fixes

--- frontend/src/utilities/axios.js
+++ frontend/src/utilities/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5555/api/v1" : "https://codepy-v2-backend.onrender.com/api/v1",
+    baseURL: "https://codepy-v2-backend.onrender.com/api/v1/",
     withCredentials: true
 });
