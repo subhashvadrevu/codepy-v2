e47fdf4aef6941a74e6848ad09784d114bca0756
Subhash <146863118+subhashvadrevu@users.noreply.github.com>
2025-06-07 19:10:30 +0530

    fixes

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,6 +1,5 @@
 import bcrypt from "bcryptjs";
 import { db } from "../libs/db.js"
-import { UserRole } from "../generated/prisma/index.js";
 import jwt from "jsonwebtoken";
 
 
@@ -28,7 +27,7 @@ export const register = async(req, res) => {
                 username: email.split("@")[0],
                 password: hashedPassword,
                 name,
-                role: UserRole.USER
+                role: "USER"
             }
         });
 
