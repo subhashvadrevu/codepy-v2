489fd97317b34e3dc190a289dd06dea229d8f0ba
Subhash <146863118+subhashvadrevu@users.noreply.github.com>
2025-06-07 18:19:17 +0530

    fixes

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://codepy-v2-frontend.onrender.com/",
+    origin: "https://codepy-v2-frontend.onrender.com/*",
     credentials: true
 }));
 
