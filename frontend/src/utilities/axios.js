bf76bca7ac45f2035283f9636378ceb1413a72fd
Subhash <146863118+subhashvadrevu@users.noreply.github.com>
2025-06-07 19:42:11 +0530

    fixes

--- frontend/src/utilities/axios.js
+++ frontend/src/utilities/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
     baseURL: "https://codepy-v2-backend.onrender.com/api/v1",
-    withCredentials: false,
+    withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
     }
