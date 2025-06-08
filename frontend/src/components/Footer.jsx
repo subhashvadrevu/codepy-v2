826437687f9cbc4438ce6020238c3954f5e10367
Subhash <146863118+subhashvadrevu@users.noreply.github.com>
2025-06-08 11:08:57 +0530

    footer update

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="relative bottom-0 text-center text-gray-600 text-sm py-4 dark:text-gray-200">
+    <div className="bg-white dark:bg-[#18181a] relative bottom-0 text-center text-gray-600 text-sm py-4 dark:text-gray-200">
   © 2025 CodePy • Built by <a href="https://github.com/subhashvadrevu" target="_blank" rel="noopener noreferrer" className="text-[#ff6900] hover:underline">Subhash</a>
 </div>
 
