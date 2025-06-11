88b41df3bdf09ba1ced5dd9d0e4246826de0aa18
Subhash <146863118+subhashvadrevu@users.noreply.github.com>
2025-06-11 16:13:29 +0530

    changes & updates

--- frontend/src/utilities/axios.js
+++ frontend/src/utilities/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://codepy-v2-backend.onrender.com/api/v1",
+    baseURL:  "http://localhost:5555/api/v1",
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
--- frontend/src/utilities/zodSchema.js
+++ frontend/src/utilities/zodSchema.js
@@ -17,6 +17,12 @@ export const problemSchema = z.object({
   description: z.string().min(10, "Description must be at least 10 characters"),
   difficulty: z.enum(["EASY", "MEDIUM", "HARD"]),
   constraints: z.string().min(1, "Constraints are required"),
+  tags: z.array(
+    z.object({
+      label: z.string(),
+      value: z.string(),
+    })
+  ).min(1, "Atleast one tag must be selected"),
   hints: z.string().optional(),
   editorial: z.string().optional(),
   testcases: z
@@ -27,31 +33,32 @@ export const problemSchema = z.object({
       })
     )
     .min(1, "At least one test case is required"),
-  examples: z.object({
-    JAVASCRIPT: z.object({
-      input: z.string().min(1, "Input is required"),
-      output: z.string().min(1, "Output is required"),
-      explanation: z.string().optional(),
-    }),
-    PYTHON: z.object({
-      input: z.string().min(1, "Input is required"),
-      output: z.string().min(1, "Output is required"),
-      explanation: z.string().optional(),
-    }),
-    JAVA: z.object({
-      input: z.string().min(1, "Input is required"),
-      output: z.string().min(1, "Output is required"),
-      explanation: z.string().optional(),
-    }),
-  }),
+  examples: z
+    .array(
+      z.object({
+        input: z.string().min(1, "Input is required"),
+        output: z.string().min(1, "Output is required"),
+        // explantion: z.string().min(1, "Explantion is required"),
+      })
+    )
+    .min(1, "At least one test case is required"),
   codeSnippets: z.object({
-    JAVASCRIPT: z.string().min(1, "JavaScript code snippet is required"),
+    // JAVASCRIPT: z.string().min(1, "JavaScript code snippet is required"),
     PYTHON: z.string().min(1, "Python code snippet is required"),
-    JAVA: z.string().min(1, "Java solution is required"),
+    // JAVA: z.string().min(1, "Java solution is required"),
+  }),
+  referenceSolutions: z.object({
+    // JAVASCRIPT: z.string().min(1, "JavaScript code snippet is required"),
+    PYTHON: z.string().min(1, "Python refernce solution is required"),
+    // JAVA: z.string().min(1, "Java solution is required"),
   }),
 });
 
 
 export const deleteProblemSchema = z.object({
   problemId: z.string().min(1, "Problem ID is required"),
+});
+
+export const verifyOtpSchema = z.object({
+  otp: z.string().length(6, "Invalid OTP"),
 });
