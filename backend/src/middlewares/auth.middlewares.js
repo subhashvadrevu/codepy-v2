c9eb1ed34adaa730e1ca5c7a310631511297f5eb
Subhash <146863118+subhashvadrevu@users.noreply.github.com>
2025-06-11 17:03:06 +0530

    fixes

--- backend/src/middlewares/auth.middlewares.js
+++ backend/src/middlewares/auth.middlewares.js
@@ -3,7 +3,7 @@ import  { db } from "../libs/db.js";
 
 export const authMiddleware = async(req, res, next) => {
     try {
-        const token = req.cookie.jwt;
+        const token = req.cookies.jwt;
 
         if(!token) {
             return res.status(401).json({
