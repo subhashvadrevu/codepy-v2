00daf2657c3b8977c1c6bf6ae8090a04916ee66e
Subhash <146863118+subhashvadrevu@users.noreply.github.com>
2025-10-05 14:01:08 +0530

    added health check route

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,9 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: ["https://codepy-v2.vercel.app", "https://www.codepy.live", "http://localhost:5173"],
+    // origin: 'http://localhost:5173',
+    // credentials: true,
+    origin: ["https://codepy-v2.vercel.app", "https://www.codepy.live"],
     credentials: true
 }));
 
@@ -23,6 +25,18 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/problems", problemRoutes);
 app.use("/api/v1/submit", submissionRoutes);
 app.use("/api/v1/list", listRoutes);
+app.get("/api/v1/healthCheck", (req, res) => {
+    try {
+        return res.status(200).json({
+            success: true,
+            message: "Manchiga unna bidda"
+        });
+    } catch (error) {
+        return res.status(500).json({
+            error: "emo mama, halath karab ayindi"
+        });
+    }
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`server is running on port ${process.env.PORT}`);
