1044ffc071602e693a9e9748bbe7196b556cd6cc
Subhash <146863118+subhashvadrevu@users.noreply.github.com>
2025-10-14 03:29:28 +0530

    cors resolved

--- frontend/src/utilities/axios.js
+++ frontend/src/utilities/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://codepy-v2.onrender.com/api/v1", 
+    baseURL: "https://codepy-v2.onrender.com/", 
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
