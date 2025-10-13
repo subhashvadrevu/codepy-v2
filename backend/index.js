5ee0edc2e8cab9cd7dc1133c1000590aebe1525a
Subhash <146863118+subhashvadrevu@users.noreply.github.com>
2025-10-14 03:22:24 +0530

    cors resolved

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://www.codepy.live',
     credentials: true,
     // origin: ["https://codepy-v2.vercel.app", "https://www.codepy.live"],
     // credentials: true
